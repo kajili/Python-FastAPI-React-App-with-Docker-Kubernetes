b021b1716cce4710d9d9e974b55ac4bd71407d7b
Kevin Ajili <kajili@ucsc.edu>
2021-06-05 21:12:34 -0700

    completed basic python function that gathers dog facts data from API

--- python_code/main.py
+++ python_code/main.py
@@ -1,8 +1,21 @@
-from fastapi import FastAPI
+# from fastapi import FastAPI
 
-app = FastAPI()
+# app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+# @app.get("/")
+# async def root():
+#     return {"message": "Hello World"}
+
+import requests
+
+def get_dog_facts_data():
+    response = requests.get("https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?number=10")
+
+    if response.ok:
+        return response.json()
+    else:
+        return {"error": "API Request failed"}
+
+
+print(get_dog_facts_data())
