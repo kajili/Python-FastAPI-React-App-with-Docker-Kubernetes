416c68cb01f6285f6a20b28c56b4fd229c884945
Kevin Ajili <kajili@ucsc.edu>
2021-06-06 19:27:15 -0700

    succesfully displaying the list of Dog Facts through the DogFacts component within App.js.

--- react_code/src/components/DogFacts.js
+++ react_code/src/components/DogFacts.js
@@ -1,5 +1,17 @@
 import React from "react";
+import { List, Header } from "semantic-ui-react";
 
 export const DogFacts = ({ dogFacts }) => {
-  return <div>{dogFacts.length}</div>;
+  return (
+    <List>
+      {dogFacts.map((dogFact) => {
+        return (
+          <List.Item key={dogFact.fact}>
+            <Header>{dogFact.fact}</Header>
+            <br />
+          </List.Item>
+        );
+      })}
+    </List>
+  );
 };
